569b55a1ff8ce9a28e54895dc1b0b9f47973a2b3
vkletsko <v.kletsko@gmail.com>
2024-06-09 14:54:56 +0000

    Deploying to gh-pages from @ vkletsko/goit-advancedjs-hw-02@ccaf9d41f9ce96d1b9d2905b6d756560c2254040 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const r=document.querySelector("[data-start]"),o=document.querySelector("[data-stop]");let a=null;const t=e=>{const n=e.disabled;e.disabled=!n},c=()=>`#${Math.floor(Math.random()*16777215).toString(16).padStart(6,0)}`,l=({interval:e})=>{a=setInterval(()=>{const n=c();document.body.style.background=n},e)};t(o);r.addEventListener("click",()=>{t(o),t(r),l({interval:1e3})});o.addEventListener("click",()=>{t(r),t(o),clearInterval(a)});
+//# sourceMappingURL=commonHelpers.js.map
